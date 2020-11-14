4c0dc8d55d83d8d6878b4b825fad6da820f5dee0
Raquel Poff <king_raquel@ymail.com>
2020-11-13 19:49:16 -0600

    Done!!!!!! For real

--- queries.sql
+++ queries.sql
@@ -1,7 +1,18 @@
 ## Part 1: Test it with SQL
 
+id int
+employer VARCHAR
+
 ## Part 2: Test it with SQL
+
+SELECT name form employer where location = St Louis City
 
 ## Part 3: Test it with SQL
 
+TABLE DELETE job;
+
 ## Part 4: Test it with SQL
+
+SELECT name, description from skill
+WHERE  job_skill  IS NOT NULL
+ORDER BY job_name ASC
